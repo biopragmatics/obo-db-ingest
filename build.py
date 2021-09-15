6529d3ddfcacb09ae55f58ec07eb2cef262948f2
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-09-15 15:39:06 +0200

    Update build and add new examples

--- build.py
+++ build.py
@@ -9,26 +9,48 @@ import os
 
 import click
 from more_click import verbose_option
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
-from pyobo.sources import cgnc, drugbank, drugbank_salt, drugcentral, hgnc, hgncgenefamily, mgi, rgd, sgd
+from pyobo.sources import (
+    cgnc, dictybase_gene, hgnc, hgncgenefamily, mgi,
+    pombase, rgd, sgd, zfin,
+)
 
 HERE = os.path.abspath(os.path.dirname(__file__))
+MODULES = [
+    hgncgenefamily,
+    # Organism-specific gene nomenclature examples
+    hgnc,
+    mgi,
+    # araport
+    rgd,
+    sgd,
+    pombase,
+    # wormbase
+    dictybase_gene,
+    # ecogene
+    # flybase
+    zfin,
+    cgnc,
+    # Chemistry examples
+    # drugcentral,
+    # drugbank,
+    # drugbank_salt,
+]
 
 
 @click.command()
 @verbose_option
 def main():
     """Build the PyOBO examples."""
-    hgnc.get_obo(force=True).write_obo(os.path.join(HERE, 'hgnc.obo'))
-    hgncgenefamily.get_obo(force=True).write_obo(os.path.join(HERE, 'hgnc_genegroup.obo'))
-    mgi.get_obo(force=True).write_obo(os.path.join(HERE, 'mgi.obo'))
-    rgd.get_obo(force=True).write_obo(os.path.join(HERE, 'rgd.obo'))
-    sgd.get_obo(force=True).write_obo(os.path.join(HERE, 'sgd.obo'))
-    drugbank.get_obo(force=True).write_obo(os.path.join(HERE, 'drugbank.obo'))
-    # FIXME cgnc export is broken
-    cgnc.get_obo(force=True).write_obo(os.path.join(HERE, 'cgnc.obo'))
-    drugbank_salt.get_obo(force=True).write_obo(os.path.join(HERE, 'drugbank_salt.obo'))
-    drugcentral.get_obo(force=True).write_obo(os.path.join(HERE, 'drugcentral.obo'))
+    it = tqdm(MODULES, desc="Making OBO examples")
+    for module in it:
+        name = module.__name__.split(".")[-1]
+        it.set_postfix(name=name)
+        with logging_redirect_tqdm():
+            obo = module.get_obo(force=True)
+            obo.write_obo(os.path.join(HERE, f'{name}.obo'))
 
 
 if __name__ == '__main__':
