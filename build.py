57c99e0b7373e3fd2e0a6f5403eeff06f57f66b3
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-09-15 02:41:04 +0200

    Update examples

--- build.py
+++ build.py
@@ -14,6 +14,7 @@ import pyobo
 import pyobo.sources.drugbank
 import pyobo.sources.drugcentral
 import pyobo.sources.hgnc
+import pyobo.sources.hgncgenefamily
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,13 +23,16 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 @verbose_option
 def main():
     """Build the PyOBO examples."""
-    hgnc_obo = pyobo.sources.hgnc.get_obo()
+    hgnc_obo = pyobo.sources.hgnc.get_obo(force=True)
     hgnc_obo.write_obo(os.path.join(HERE, 'hgnc.obo'))
 
-    drugbank_obo = pyobo.sources.drugbank.get_obo()
+    hgnc_group_obo = pyobo.sources.hgncgenefamily.get_obo(force=True)
+    hgnc_group_obo.write_obo(os.path.join(HERE, 'hgnc_genegroup.obo'))
+
+    drugbank_obo = pyobo.sources.drugbank.get_obo(force=True)
     drugbank_obo.write_obo(os.path.join(HERE, 'drugbank.obo'))
 
-    drugcentral_obo = pyobo.sources.drugcentral.get_obo()
+    drugcentral_obo = pyobo.sources.drugcentral.get_obo(force=True)
     drugcentral_obo.write_obo(os.path.join(HERE, 'drugcentral.obo'))
 
 
