dd1d443a08a96ba4a120b51af128a4950ccccf0c
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-09-15 23:48:01 +0200

    Add first set of OBO Graph JSONs

--- build.py
+++ build.py
@@ -16,6 +16,7 @@ from pyobo.sources import (
     cgnc, dictybase_gene, drugbank, drugbank_salt, drugcentral, flybase, hgnc, hgncgenefamily, mgi, pombase, rgd, sgd,
     zfin,
 )
+from pyobo.utils.misc import obo_to_obograph
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 MODULES = [
@@ -47,10 +48,19 @@ def main():
     it = tqdm(MODULES, desc="Making OBO examples")
     for module in it:
         name = module.__name__.split(".")[-1]
+        obo_path = os.path.join(HERE, f'{name}.obo')
+        obograph_path = os.path.join(HERE, f'{name}.json.gz')
+
         it.set_postfix(name=name)
         with logging_redirect_tqdm():
             obo = module.get_obo(force=True)
-            obo.write_obo(os.path.join(HERE, f'{name}.obo'))
+            obo.write_obo(obo_path)
+
+        try:
+            obo_to_obograph(obo_path, obograph_path)
+        except Exception as e:
+            it.write(click.style(f"{name} failed to convert to OBO Graph", fg="red"))
+            it.write(click.style(str(e), fg="red"))
 
 
 if __name__ == '__main__':
