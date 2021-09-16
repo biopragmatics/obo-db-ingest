c9770ab445590cadccebecbef20642981e6750fe
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-09-16 02:50:33 +0200

    Reorganize

--- build.py
+++ build.py
@@ -21,21 +21,21 @@ from pyobo.utils.misc import obo_to_obograph
 
 HERE = Path(__file__).parent.resolve()
 MODULES = [
-    # hgncgenefamily,
-    # # Organism-specific gene nomenclature examples
-    # hgnc,
-    # mgi,
-    # # araport
-    # rgd,
-    # sgd,
-    # pombase,
-    # # wormbase
-    # dictybase_gene,
-    # # ecogene
-    # flybase,
-    # zfin,
-    # cgnc,
-    # mirbase,
+    hgncgenefamily,
+    # Organism-specific gene nomenclature examples
+    hgnc,
+    mgi,
+    # araport
+    rgd,
+    sgd,
+    pombase,
+    # wormbase
+    dictybase_gene,
+    # ecogene
+    flybase,
+    zfin,
+    cgnc,
+    mirbase,
     uniprot,
     # Chemistry examples
     drugcentral,
@@ -49,24 +49,30 @@ MODULES = [
 def main():
     """Build the PyOBO examples."""
     it = tqdm(MODULES, desc="Making OBO examples")
-    for module in it:
-        name = module.__name__.split(".")[-1]
-        it.set_postfix(name=name)
+    with logging_redirect_tqdm():
+        for module in it:
+            prefix = module.PREFIX
+            it.set_postfix(prefix=prefix)
 
-        directory = HERE.joinpath(name)
-        directory.mkdir(exist_ok=True, parents=True)
-        obo_path = directory.joinpath(f'{name}.obo')
-        obograph_path = directory.joinpath(f'{name}.json.gz')
-
-        with logging_redirect_tqdm():
             obo = module.get_obo(force=True)
+
+            directory = HERE.joinpath("export", prefix)
+            if obo.data_version:
+                directory = HERE.joinpath(directory, obo.data_version)
+            else:
+                tqdm.write(click.style(f"{prefix} has no version info", fg="red"))
+            directory.mkdir(exist_ok=True, parents=True)
+            obo_path = directory.joinpath(f'{prefix}.obo')
+            obograph_path = directory.joinpath(f'{prefix}.json.gz')
+
             obo.write_obo(obo_path)
 
-        try:
-            obo_to_obograph(obo_path, obograph_path)
-        except Exception as e:
-            it.write(click.style(f"{name} failed to convert to OBO Graph", fg="red"))
-            it.write(click.style(str(e), fg="red"))
+            try:
+                it.write(f"converting {prefix} to OBO graph")
+                obo_to_obograph(obo_path, obograph_path)
+            except Exception as e:
+                it.write(click.style(f"{prefix} failed to convert to OBO Graph", fg="red"))
+                it.write(click.style(str(e), fg="red"))
 
 
 if __name__ == '__main__':
