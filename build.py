f6214036463ec2716aaf28a38632a306dbb4f9ba
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-06-22 15:54:45 +0200

    Update MGI and RGD

--- build.py
+++ build.py
@@ -24,8 +24,10 @@ from pyobo.sources import (
     flybase,
     hgnc,
     hgncgenefamily,
+    mgi,
     mirbase,
     pombase,
+    rgd,
     rhea,
     sgd,
     zfin,
@@ -39,9 +41,8 @@ MODULES = [
     hgncgenefamily,
     # Organism-specific gene nomenclature examples
     hgnc,
-    # mgi,
+    mgi,
     # araport
-    # rgd,
     sgd,
     pombase,
     # wormbase
@@ -60,21 +61,33 @@ MODULES = [
     #
     complexportal,
     expasy,
+    rgd,
 ]
 
 NO_FORCE = {drugbank, drugbank_salt}
+GZIP_OBO = {"mgi", "uniprot"}
+
+
+def _gzip(path: Path, suffix: str):
+    output_path = path.with_suffix(suffix)
+    with path.open("rb") as src, gzip.open(output_path, "wb") as dst:
+        dst.writelines(src)
+    path.unlink()
 
 
 @click.command()
 @verbose_option
 @click.option("-m", "--minimum")
-def main(minimum: Optional[str]):
+@click.option("-x", "--xvalue")
+def main(minimum: Optional[str], xvalue: Optional[str]):
     """Build the PyOBO examples."""
     it = tqdm(sorted(MODULES, key=attrgetter("PREFIX")), desc="Making OBO examples")
     with logging_redirect_tqdm():
         for module in it:
             prefix = module.PREFIX
             if minimum and prefix.lower() < minimum.lower():
+                continue
+            if xvalue and xvalue.lower() != prefix.lower():
                 continue
             it.set_postfix(prefix=prefix)
 
@@ -92,6 +105,8 @@ def main(minimum: Optional[str]):
 
             try:
                 obo.write_obo(obo_path)
+                if prefix in GZIP_OBO:
+                    _gzip(obo_path, ".obo.gz")
             except ValueError as e:
                 it.write(click.style(f"[{prefix}] failed to write OBO: {e}", fg="red"))
                 continue
@@ -99,32 +114,22 @@ def main(minimum: Optional[str]):
             try:
                 it.write(f"[{prefix}] converting to OBO Graph JSON")
                 convert_to_obograph(input_path=obo_path, json_path=obo_graph_json_path)
-                with obo_graph_json_path.open("rb") as src, gzip.open(
-                    obo_graph_json_path.with_suffix(".json.gz"), "wb"
-                ) as dst:
-                    dst.writelines(src)
+                _gzip(obo_graph_json_path, ".json.gz")
             except Exception:
                 it.write(
                     click.style(
                         f"[{prefix}] ROBOT failed to convert to OBO Graph", fg="red"
                     )
                 )
-            else:
-                obo_graph_json_path.unlink()
 
             try:
                 it.write(f"[{prefix}] converting to OWL")
                 convert(obo_path, owl_path)
-                with owl_path.open("rb") as src, gzip.open(
-                    owl_path.with_suffix(".owl.gz"), "wb"
-                ) as dst:
-                    dst.writelines(src)
+                _gzip(owl_path, ".owl.gz")
             except Exception:
                 it.write(
                     click.style(f"[{prefix}] ROBOT failed to convert to OWL", fg="red")
                 )
-            else:
-                owl_path.unlink()
 
 
 if __name__ == "__main__":
